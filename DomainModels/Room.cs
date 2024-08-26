087638b6cc93b1cb51cd1767ed4b5517838cf885
Simi0001 <simi0001@edu.mercantec.dk>
2024-08-26 09:55:03 +0200

    make rooms class

---
+++ DomainModels/Room.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainModels
+{
+    private enum Status
+    {
+        available,
+        underMaintenece,
+        needsCleaning
+
+    }
+
+    internal class Room
+    {
+
+        private int Id {  get; set; }
+        public string Type { get; set; }
+        public int Price { get; set; }
+
+        private List<DateTime> BookedDays { get; set; } = new List<DateTime>();
+
+
+    }
+}
