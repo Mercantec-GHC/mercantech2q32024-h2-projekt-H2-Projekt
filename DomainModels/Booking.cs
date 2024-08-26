cee1f0438fd9960c53759354fcc871dde12ca240
simo100f <simo100f@edu.mercantec.dk>
2024-08-26 09:44:35 +0200

    booking class created

---
+++ DomainModels/Booking.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainModels
+{
+    internal class Booking
+    {
+        List<int> rooms { get; set; } = new List<int>();
+        string guestName { get; set; }
+        string guestEmail { get; set; }
+        string guestPhoneNr { get; set; }
+
+        public void GetRoomAvailability()
+        {
+
+        }
+    }
+}
