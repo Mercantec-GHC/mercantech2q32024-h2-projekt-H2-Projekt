6aa39d53299b421ff7b58ebcebb57d4b6e3e7d53
StineDk <144903306+StineDk@users.noreply.github.com>
2024-08-26 09:23:38 +0200

    Create User.cs
    Created the class, and made the empty functions

---
+++ DomainModels/User.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainModels
+{
+	internal class User
+	{
+		private string userName { get; set; } = null!;
+		private string password { get; set; } = null!;
+		public string name { get; set; } = null!;
+		private string? phoneNr { get; set; }
+		private string? email { get; set; }
+		private List<int> bookingIds { get; set; } = new List<int>();
+
+		public void DeleteOwnGuestAccount()
+		{ }
+
+		public void CreateOwnGuestAccount(string userName, string password)
+		{ }
+
+		public void AddTickets()
+		{ }
+
+		public void CreateBooking()
+		{ }
+
+		public void DeleteOwnBooking()
+		{ }
+
+		public void AddOwnBooking()
+		{ }
+	}
+}
