acfd702f68bd2e32cb472d003f57f0903fb09de5
Jonas <JMikkelsen40@outlook.com>
2024-08-27 12:57:27 +0200

    Added the rest of the models
    Added the RoomType, PentHouse, PremiumHouse, STandardHouse and Extra models

---
+++ DomainModels/PremiumHouse.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainModels
+{
+    public class PremiumHouse
+    {
+        public int Id { get; set; }
+        public List<string> facilitates = new List<string>()
+        {
+            "Dobbeltseng", "badeværelse med badekar og bruser", "TV", "skrivebord", "minibar", "Wi-Fi", "balkon"
+        };
+        public decimal PricePerNight;
+
+        public string numberOfRooms { get; set; }
+        public decimal CalRoomPrice(PremiumHouse rooms)
+        {
+            if (rooms.numberOfRooms == "single room")
+            {
+                rooms.PricePerNight = 900;
+            }
+            else if (rooms.numberOfRooms == "double room")
+            {
+                rooms.PricePerNight = 1200;
+            }
+            else
+            {
+                rooms.PricePerNight = 0;
+            }
+            return rooms.PricePerNight;
+        }
+    }
+
+
+}
