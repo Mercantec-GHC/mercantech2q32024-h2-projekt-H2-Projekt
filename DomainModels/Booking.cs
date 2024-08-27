5392412cd1e1b70e7b097c42197552e2310d2f87
Simi0001 <simi0001@edu.mercantec.dk>
2024-08-27 12:18:52 +0200

    Changede class location

---
+++ DomainModels/Booking.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API.Models
+{
+    internal class Booking
+    {
+        List<Room> rooms { get; set; } = new List<Room>();
+        string guestName { get; set; }
+        string guestEmail { get; set; }
+        string guestPhoneNr { get; set; }
+        List<DateOnly> bookingDates { get; set; } = new List<DateOnly>();
+
+        public void GetRoomAvailability()
+        {
+
+        }
+    }
+}
