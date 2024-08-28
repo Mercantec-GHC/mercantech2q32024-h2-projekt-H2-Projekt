a6e08e75eb1069ce13035e2ab2b9e04be08a32f9
Lukas11n <luno0001@edu.mercantec.dk>
2024-08-28 09:30:39 +0200

    Mail Done

--- DomainModels/Administrator.cs
+++ DomainModels/Administrator.cs
@@ -9,7 +9,7 @@ namespace DomainModels
     internal class Administrator : Receptionist
     {
         public void DeleteAnyAccount() { }
-        public void AddAnyAccount() { }
+        public void AddAnyAccount(string userName, string Password) { }
         public void CreateRoom() { }
         public void ViewOccupancyReport() { }
 
