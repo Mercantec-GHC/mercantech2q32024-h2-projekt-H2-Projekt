5b7dbda967a3344d8d7cbbf12ffcdf231c8443a4
Mape0009 <mape0009@edu.mercantec.dk>
2024-08-28 14:17:14 +0200

    Udkast til room endpoint

---
+++ API/Controllers/roomEndpoint.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using DomainModels;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class roomEndpoint : ControllerBase
+    {
+        private readonly HotelContext _context;
+        public roomEndpoint(HotelContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public IActionResult GetAll() 
+        {
+            var room = _context.Room.ToList();
+            
+            return Ok(room);
+        }
+
+        [HttpGet("{Id}")]
+
+        public IActionResult GetById([FromRoute] int Id) 
+        {
+            var room = _context.Room.FindAsync(Id);
+            if (room == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(room);
+        }
+    }
+}
