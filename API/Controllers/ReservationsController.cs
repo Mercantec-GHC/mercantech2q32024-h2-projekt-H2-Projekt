5aef3509ee67e3e2d390ec31ecca111bef9ecf0e
Rasmus Tanggaard <rasmus@tthost.dk>
2024-08-29 12:08:09 +0200

    Add documentation

--- API/Controllers/ReservationsController.cs
+++ API/Controllers/ReservationsController.cs
@@ -5,6 +5,11 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
+    /// <summary>
+    /// Controller for reservation endpoints
+    /// 
+    /// Most likely wanna later add some more complex logic here to handle creation of reservations based off of user and room availability and such.
+    /// </summary>
     [ApiController]
     [Route("v1/api/[controller]/[action]")]
     public class ReservationsController : Controller
@@ -14,42 +19,72 @@ namespace API.Controllers
 
         public ReservationsController(HotelContext context)
         {
+            // Fill the DB Context through Dependency Injection
             _context = context;
         }
 
+        /// <summary>
+        /// Get all reservations
+        /// </summary>
+        /// <returns>Status OK with the list of reservations</returns>
         [HttpGet]
         public IActionResult Get()
         {
-           var reservations = _context.Reservations.ToList();
+            // Currently the simplest CRUD operation
+            var reservations = _context.Reservations.ToList();
             return Ok(reservations);
         }
 
+        /// <summary>
+        /// Get specific reservation by ID
+        /// </summary>
+        /// <param name="id">Reservation ID</param>
+        /// <returns>Status OK with reservation</returns>
         [HttpGet("{id}")]
         public IActionResult Get(int id)
         {
+            // Currently the simplest CRUD operation
             var reservation = _context.Reservations.Find(id);
             return Ok(reservation);
         }
 
+        /// <summary>
+        /// Add new reservation
+        /// </summary>
+        /// <param name="reservation">Reservation object</param>
+        /// <returns>Status CREATED</returns>
         [HttpPost]
         public IActionResult Post([FromBody] Reservation reservation)
         {
+            // Currently the simplest CRUD operation
             _context.Reservations.Add(reservation);
             _context.SaveChanges();
             return Created();
         }
 
+        /// <summary>
+        /// Update reservation
+        /// </summary>
+        /// <param name="reservation">Reservation object</param>
+        /// <returns>Status OK with the modified reservation</returns>
         [HttpPut]
         public IActionResult Update([FromBody] Reservation reservation)
         {
+            // Currently the simplest CRUD operation
             _context.Reservations.Update(reservation);
             _context.SaveChanges();
             return Ok(reservation);
         }
 
+        /// <summary>
+        /// Delete reservation by ID
+        /// </summary>
+        /// <param name="id">Integer ID of reservation</param>
+        /// <returns>Status OK</returns>
         [HttpDelete("{id}")]
         public IActionResult Delete(int id)
         {
+            // Currently the simplest CRUD operation
             var reservation = _context.Reservations.Find(id);
             _context.Reservations.Remove(reservation);
             _context.SaveChanges();
