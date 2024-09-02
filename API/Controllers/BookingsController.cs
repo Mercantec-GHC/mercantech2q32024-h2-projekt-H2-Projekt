ddd87e83f19c3c271dcc29e93e1535804ae75900
yevheniia.zhygalova <YevaITUA@gmail.com>
2024-09-02 13:34:28 +0200

    Add Booking Controller

--- API/Controllers/BookingsController.cs
+++ API/Controllers/BookingsController.cs
@@ -16,17 +16,21 @@ namespace API.Controllers
 			_hotelContext = hotelContext;
 		}
 
-		[HttpGet]
+		[HttpGet("all")]
 		public async Task<ActionResult<IEnumerable<Booking>>> GetBookings()
 		{
-			var bookings = await _hotelContext.Bookings.ToArrayAsync();
+			var bookings = await _hotelContext.Bookings.Include(b => b.Room).ToArrayAsync();
 			return Ok(bookings);
 		}
 
-		[HttpGet("id/{BookingId}")]
+        [HttpGet("id/{BookingId}")]
         public async Task<ActionResult<Booking>> GetBookingById(int BookingId)
         {
-            var booking = await _hotelContext.Bookings.FindAsync(BookingId);
+            
+            var booking = await _hotelContext.Bookings
+                .Where(b => b.BookingId == BookingId)
+                .Include(b => b.Room)
+                .FirstOrDefaultAsync();
             if (booking == null)
             {
                 return NotFound();
@@ -38,7 +42,10 @@ namespace API.Controllers
 		[HttpGet("name/{GuestName}")]
         public async Task<ActionResult<Booking>> GetBookingByGuestName(string GuestName)
         {
-            var booking = await _hotelContext.Bookings.FindAsync(GuestName);
+            var booking = await _hotelContext.Bookings
+                .Where(b => b.GuestName == GuestName)
+                .Include(b => b.Room)
+                .FirstOrDefaultAsync();
             if (booking == null)
             {
                 return NotFound();
@@ -50,7 +57,10 @@ namespace API.Controllers
 		[HttpGet("email/{GuestEmail}")]
         public async Task<ActionResult<Booking>> GetBookingByGuestEmail(string GuestEmail)
         {
-            var booking = await _hotelContext.Bookings.FindAsync(GuestEmail);
+            var booking = await _hotelContext.Bookings
+                .Where(b => b.GuestEmail == GuestEmail)
+                .Include(b => b.Room)
+                .FirstOrDefaultAsync();
             if (booking == null)
             {
                 return NotFound();
@@ -62,7 +72,10 @@ namespace API.Controllers
         [HttpGet("phone/{GuestPhoneNr}")]
         public async Task<ActionResult<Booking>> GetBookingByGuestPhoneNr(string GuestPhoneNr)
         {
-            var booking = await _hotelContext.Bookings.FindAsync(GuestPhoneNr);
+            var booking = await _hotelContext.Bookings
+                .Where(b => b.GuestPhoneNr == GuestPhoneNr)
+                .Include(b => b.Room)
+                .FirstOrDefaultAsync();
             if (booking == null)
             {
                 return NotFound();
@@ -73,11 +86,113 @@ namespace API.Controllers
 
        
 
-        [HttpPost]
-		public void PostBooking(Booking booking)
+        [HttpPost("add")]
+        [ProducesResponseType(201)]
+		public ActionResult AddBooking(CreateBookingDTO bookingDTO)
 		{
-			_hotelContext.Bookings.Add(booking);
+            var room = _hotelContext.Rooms.Find(bookingDTO.RoomId);
+            if (room == null)
+            {
+                return NotFound("room not found");
+            }
+
+            var user = _hotelContext.Users.Find(bookingDTO.UserId);
+            if (user == null)
+            {
+                return NotFound("user not found");
+            }
+
+            if (bookingDTO.DateFrom >= bookingDTO.DateTo)
+            {
+                return BadRequest("Invalid date range");
+            }
+
+            if (bookingDTO.DateFrom < DateTime.Now)
+            {
+                return BadRequest("Invalid date range");
+            }
+
+
+            var booking = new Booking
+            {
+                Room = room,
+                GuestName = user.FullName,
+                GuestEmail = user.Email,
+                GuestPhoneNr = user.PhoneNr,
+                BookingDates = new List<DateTime> { bookingDTO.DateFrom, bookingDTO.DateTo }
+            };
+            _hotelContext.Bookings.Add(booking);
 			_hotelContext.SaveChanges();
-		}
-	}
+
+            return Ok("Done");
+        }
+
+        [HttpPut("update")]
+        [ProducesResponseType(204)]
+        public ActionResult UpdateBooking(UpdateBookingDTO bookingDTO)
+        {
+
+            if (bookingDTO.BookingId <= 0)
+            {
+                return BadRequest("Invalid booking id");
+            }
+
+            var booking = _hotelContext.Bookings.Find(bookingDTO.BookingId);
+            if (booking == null)
+            {
+                return NotFound("booking not found");
+            }
+
+            var room = _hotelContext.Rooms.Find(bookingDTO.RoomId);
+
+
+            if (room == null)
+            {
+                return NotFound("room not found");
+            }
+
+            var user = _hotelContext.Users.Find(bookingDTO.UserId);
+            if (user == null)
+            {
+                return NotFound("user not found");
+            }
+
+            if (bookingDTO.DateFrom >= bookingDTO.DateTo)
+            {
+                return BadRequest("Invalid date range");
+            }
+
+            if (bookingDTO.DateFrom < DateTime.Now)
+            {
+                return BadRequest("Invalid date range");
+            }
+
+            booking.Room = room;
+            booking.GuestName = user.FullName;
+            booking.GuestEmail = user.Email;
+            booking.GuestPhoneNr = user.PhoneNr;
+            booking.BookingDates = new List<DateTime> { bookingDTO.DateFrom, bookingDTO.DateTo };
+          
+            _hotelContext.SaveChanges();
+
+            return Ok("Done");
+        }
+
+
+
+        [HttpDelete("id/{BookingId}")]
+        public async Task<ActionResult<Booking>> DeleteBooking(int BookingId)
+        {
+            var booking = await _hotelContext.Bookings.FindAsync(BookingId);
+            if (booking == null)
+            {
+                return NotFound();
+            }
+
+            _hotelContext.Bookings.Remove(booking);
+            await _hotelContext.SaveChangesAsync();
+
+            return Ok(booking);
+        }
+    }
 }
