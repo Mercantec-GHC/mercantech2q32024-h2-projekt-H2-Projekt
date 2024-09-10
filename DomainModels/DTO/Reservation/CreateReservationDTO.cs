13654d549221621a3471d241deeb1272961e7594
Rasmus Tanggaard <rasmus@tthost.dk>
2024-09-10 11:00:28 +0200

    Add authorization to modification and deletion of reservations
    Closes #67
    Closes #66

--- DomainModels/DTO/Reservation/CreateReservationDTO.cs
+++ DomainModels/DTO/Reservation/CreateReservationDTO.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +9,11 @@ namespace DomainModels.DTO
 {
     public class CreateReservationDTO
     {
+        [Required]
         public int RoomId { get; set; }
-        public string UserId { get; set; }
+        [Required]
         public DateTime CheckIn { get; set; }
+        [Required]
         public DateTime CheckOut { get; set; }
     }
 }
